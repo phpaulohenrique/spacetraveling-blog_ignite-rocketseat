a82bc19b59fdeb6afe5dc1b7f6b86a0410c62c5c
Paulo Henrique <paulo.henrique1080p@gmail.com>
2022-10-03 19:45:21 -0300

    1: correction in prismic service.

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,18 +1,7 @@
 import * as prismic from '@prismicio/client';
-import { HttpRequestLike } from '@prismicio/client';
-import { enableAutoPreviews } from '@prismicio/next';
 
-export interface PrismicConfig {
-  req?: HttpRequestLike;
-}
-
-export function getPrismicClient(config: PrismicConfig): prismic.Client {
+export function getPrismicClient(): prismic.Client {
   const client = prismic.createClient(process.env.PRISMIC_API_ENDPOINT);
-
-  enableAutoPreviews({
-    client,
-    req: config.req,
-  })
 
   return client;
 }
